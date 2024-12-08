24eb1ca0cfa8910a5b67f980278b49d9dd2c0367
Liza <lizaapostol378@gmail.com>
2024-12-08 22:11:17 +0200

    update structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
-import Profile from "./Profile/Profile";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
